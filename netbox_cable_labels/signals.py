cc54f2566217d10ec2c60726680ef63dcf700546
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2023-08-25 16:32:46 -0400

    Handle new cables separately

--- netbox_cable_labels/signals.py
+++ netbox_cable_labels/signals.py
@@ -1,13 +1,22 @@
 from .utils import render_label
 from dcim.models.cables import Cable
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
 
 @receiver(pre_save, sender=Cable)
 def handle_cable_label(instance: Cable, **kwargs):  # pylint: disable=unused-argument
     """
-    Update cable label if not defined when Cable is created or updated.
+    Update cable label if not defined when Cable is updated.
     """
-    if instance.label is None or instance.label == "":
+    if instance.pk is not None and (instance.label is None or instance.label == ""):
+        instance.label = render_label(instance)
+
+
+@receiver(post_save, sender=Cable)
+def handle_new_cable_label(instance: Cable, created: bool, **kwargs):  # pylint: disable=unused-argument
+    """
+    Update cable label if not defined when Cable is created.
+    """
+    if created and (instance.label is None or instance.label == ""):
         instance.label = render_label(instance)
